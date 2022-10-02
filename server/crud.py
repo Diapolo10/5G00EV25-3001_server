fe7e60e5269f49f1f092cbe1339738d1ef534f15
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2022-10-02 23:16:23 +0300

    [PATCH] Implemented routes

--- server/crud.py
+++ server/crud.py
@@ -1,5 +1,65 @@
 """C.R.U.D. - Create Read Update Delete"""
 
 import logging
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+
+from server import models, schemas
 
 logger = logging.getLogger(__name__)
+
+
+def get_public_rooms(db: Session):
+    return db.query(models.Room).filter(models.Room.public == True).all()
+
+
+def get_room(db: Session, room_id: UUID):
+    return db.query(models.Room).filter(models.Room.id == room_id).first()
+
+
+def create_room(db: Session, room: schemas.Room):
+    db_room = models.Room(**room.dict())
+    db.add(db_room)
+    db.commit()
+    db.refresh(db_room)
+    return db_room
+
+
+def delete_room(db: Session, room_id: UUID):
+    db.query(models.Room).filter(models.Room.id == room_id).delete()
+    db.commit()
+
+
+def get_user(db: Session, user_id: UUID):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password.get_secret_value() + "generate_salt_here_and_hash"
+    db_user = models.User(
+        email=user.email,
+        username=user.username,
+        password_hash=fake_hashed_password
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def create_message(db: Session, message: schemas.Message, room_id: UUID):
+    db_message = models.Message(**message.dict())
+    db.add(db_message)
+    db.commit()
+    db.refresh(db_message)
+    return db_message
--- server/routes/api/v1/room.py
+++ server/routes/api/v1/room.py
@@ -1,9 +1,92 @@
 """Implements room routes"""
 
 import logging
+from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from sqlalchemy.orm import Session
+
+from server import crud
+from server.database import get_db
+from server.schemas import Message, Room
 
 logger = logging.getLogger(__name__)
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/rooms'
+)
+
+
+@router.get('/', status_code=status.HTTP_200_OK, response_model=list[Room])
+async def get_public_rooms(db: Session = Depends(get_db)):
+    """Fetches the public rooms"""
+
+    logger.info("GET public chatrooms")
+
+    rooms = crud.get_public_rooms(db)
+    return rooms
+
+
+@router.post('/', status_code=status.HTTP_200_OK, response_model=Room)
+async def post_new_room(room: Room, db: Session = Depends(get_db)):
+    """Creates a new room"""
+
+    logger.info("POST new chatroom")
+
+    if crud.get_room(db, room_id=room.id) is not None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Room already exists")
+    db_room = crud.create_room(db, room=room)
+    return db_room
+
+
+@router.get('/{room_id}', status_code=status.HTTP_200_OK, response_model=Room)
+async def get_room_by_id(room_id: UUID, db: Session = Depends(get_db)):
+    """Fetches the specified room"""
+
+    logger.info("GET chatroom by ID: %s", room_id)
+
+    db_room = crud.get_room(db, room_id)
+    if db_room is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Room not found")
+    return db_room
+
+
+@router.post('/{room_id}', status_code=status.HTTP_200_OK, response_model=Message)
+async def post_message_by_id(room_id: UUID, message: Message, db: Session = Depends(get_db)):
+    """Sends a message to the room"""
+
+    logger.info("POST to chatroom ID: %s", room_id)
+
+    # NOTE: Check if message exists first
+    db_message = crud.create_message(db, message, room_id)
+    return db_message
+
+
+@router.delete('/{room_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_room_by_id(room_id: UUID, db: Session = Depends(get_db)):
+    """Deletes the specified room"""
+
+    logger.info("DELETE chatroom by ID: %s", room_id)
+
+
+@router.get('/{room_id}/{message_id}', status_code=status.HTTP_200_OK, response_model=Message)
+async def get_message_by_id(room_id: UUID, message_id: UUID, db: Session = Depends(get_db)):
+    """Fetches the specified message"""
+
+    logger.info("GET message by ID: %s", message_id)
+    return Message
+
+
+@router.put('/{room_id}/{message_id}', status_code=status.HTTP_200_OK, response_model=Message)
+async def put_message_by_id(room_id: UUID, message_id: UUID, db: Session = Depends(get_db)):
+    """Edits the specified message"""
+
+    logger.info("PUT message by ID: %s", message_id)
+    return Message
+
+
+@router.delete('/{room_id}/{message_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_message_by_id(room_id: UUID, message_id: UUID, db: Session = Depends(get_db)):
+    """Deletes the specified message"""
+
+    logger.info("DELETE message by ID: %s", message_id)
