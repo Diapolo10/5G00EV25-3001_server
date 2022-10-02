78930c69fa4310944de43d525692ba2dea063c68
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2022-10-02 23:16:23 +0300

    [PATCH] Implemented routes

--- server/schemas.py
+++ server/schemas.py
@@ -3,8 +3,13 @@
 # pylint: disable=missing-class-docstring
 
 import logging
+from datetime import datetime
+from enum import Enum, auto
+from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, SecretStr, validator
+
+from server.config import MAX_MESSAGE_LENGTH, MIN_MESSAGE_LENGTH
 
 logger = logging.getLogger(__name__)
 
@@ -13,3 +18,98 @@ class HelloWorld(BaseModel):
     """Hello, world!"""
 
     hello: str
+
+
+# Enums
+
+class AccessLevel(Enum):
+    BANNED = auto()
+    BASIC = auto()
+    VERIFIED = auto()
+    MODERATOR = auto()
+    ADMINISTRATOR = auto()
+
+
+# User models
+
+class UserBase(BaseModel):
+    """Base user model"""
+
+    id: UUID
+    username: str
+    email: str  # Consider email validator: https://pydantic-docs.helpmanual.io/usage/types/#pydantic-types
+
+
+class UserCreate(UserBase):
+    """Used when creating a new user"""
+
+    password: SecretStr
+
+
+class User(UserBase):
+    """User of the system"""
+
+    global_access_level: AccessLevel
+
+    class Config:
+        orm_mode = True
+
+
+# Chat models
+
+class Message(BaseModel):
+    """Message sent in a chatroom"""
+
+    id: UUID
+    user_id: UUID
+    message: str
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
+
+    @validator('message')
+    def message_length_acceptable(cls, value):  # pylint: disable=E0213
+        """Verify that the message length makes sense"""
+
+        if not MIN_MESSAGE_LENGTH <= len(value):
+            raise ValueError("Message is too short")
+        if not len(value) <= MAX_MESSAGE_LENGTH:
+            raise ValueError("Message is too long")
+        return value
+
+    @validator('user_id')
+    def user_id_exists(cls, value):  # pylint: disable=E0213
+        """Verify that the given user exists (just in case)"""
+
+        return value  # NOTE: Implement if needed
+
+
+class EncryptedMessage(Message):
+    """Encrypted message sent in a chatroom"""
+
+
+class Room(BaseModel):
+    """Chatroom"""
+
+    id: UUID
+    name: str
+    public: bool
+    owner: UUID | None
+
+    class Config:
+        orm_mode = True
+
+    @validator('owner')
+    def owner_exists(cls, value):  # pylint: disable=E0213
+        """Verify that the room owner exists"""
+
+        return value  # NOTE: Add validation
+
+    @validator('owner')
+    def public_if_no_owner(cls, value, values, **_):  # pylint: disable=E0213
+        """Verify that the server is public if no owner is set"""
+
+        if value is None and values['public'] is False:
+            raise ValueError("Private rooms must have an owner")
+        return value
